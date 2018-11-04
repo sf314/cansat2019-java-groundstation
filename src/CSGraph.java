eab8854972033527481a2c80c7966d92a47cdbfc
Kabir <kskapoo1@asu.edu>
2018-11-04 15:51:14 -0800

    Add functions to graph class
    The functions update,addpoints,Map and draw were added to CSgraph

--- src/CSGraph.java
+++ src/CSGraph.java
@@ -7,6 +7,21 @@ public class CSGraph extends JPanel {
 	public CSGraph() {
 		System.out.println("new CSGraph");
 	}
-	
+	public void Update()
+	{
+		
+	}
+	public void AddPoints(double a)
+	{
+		
+	}
+	public double Map(double b)
+	{
+		
+	}
+	public void Draw()
+	{
+		
+	}
 	
 }
