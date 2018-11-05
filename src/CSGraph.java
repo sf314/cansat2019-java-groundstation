01f057904acd13111c28c0fe0e758986883e974a
Kabir <kskapoo1@asu.edu>
2018-11-04 16:29:11 -0800

    Add test points

--- src/CSGraph.java
+++ src/CSGraph.java
@@ -13,15 +13,22 @@ public class CSGraph extends JPanel {
 	}
 	public void AddPoints(double a)
 	{
-		
+		System.out.println(a);
 	}
 	public double Map(double b)
 	{
-		
+		return 0.0;
 	}
 	public void Draw()
 	{
 		
 	}
-	
+	public void GenerateTestPoints()
+	{		
+		for(int i=0;i<50;i++)
+		{
+		double temp=(Math.random()*100)+1;	
+		AddPoints(temp);
+		}
+	}
 }
