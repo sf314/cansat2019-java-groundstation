d4d726df0d7ac679f366f0c281115cedee6b36bb
Ross Douglas <rdougla3@asu.edu>
2018-11-14 10:12:35 -0700

    Added Graphs to Graph Panel
    In GraphPanel class: added code to create a LinkedList of CSGraph objects and add them to the graph panel. The number of graphs created is determined by "numGraphs"
    
    In CSGraph class: added paintComponent method to draw data points (currently, the LinkedList of data points is populated by RNG so the graphs look like random scatter plots). Modified the constructor to add a label.

--- src/CSGraph.java
+++ src/CSGraph.java
@@ -6,41 +6,34 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class CSGraph extends JPanel {
+	private int lineWidth;
 	private Queue<Double> pointQueue = new LinkedList<Double>();
 	private String name;
 	private String units;
-	public CSGraph() {
-		name = "";
+	public CSGraph(String name) {
+		lineWidth = 3;
 		units = "";
+		this.name = name;
+		JLabel lab = new JLabel(name); this.add(lab);
 		System.out.println("new CSGraph");
 		GenerateTestPoints();
 		GenerateTestPoints();
 		System.out.println(pointQueue.size());
 	}
-	public void Update()
+	public void paintComponent(Graphics g)
 	{
-		
-	}
-	public void AddPoints(double a)
-	{
-		System.out.println(a);
-		pointQueue.add(a);
-		if (pointQueue.size()>200) {
-			pointQueue.remove();
+		super.paintComponent(g);
+		for(int i = 0; i<pointQueue.size(); i++)
+		{
+		g.fillOval(i, pointQueue.poll().intValue(), lineWidth, lineWidth);
 		}
 	}
-	public double Map(double b)
-	{
-		return 0.0;
+	public void AddPoints(double a){
+		System.out.println(a);
+		pointQueue.add(a);
 	}
-	public void Draw()
-	{
-		
-	}
-	public void GenerateTestPoints()
-	{		
-		for(int i=0;i<50;i++)
-		{
+	public void GenerateTestPoints(){		
+		for(int i=0;i<200;i++){
 		double temp=(Math.random()*100)+1;	
 		AddPoints(temp);
 		}
@@ -50,11 +43,18 @@ public class CSGraph extends JPanel {
 	}
 	public void setName(String name){
 		this.name = name;
+		JLabel lab = new JLabel(name); this.add(lab);
 	}
 	public String getUnits(){
 		return units;
 	}
 	public void setUnits(String units){
 		this.units = units;
+	}
+	public int getLineWidth() {
+		return lineWidth;
+	}
+	public void setLineWidth(int lineWidth) {
+		this.lineWidth = lineWidth;
 	}
 }
--- src/CSGraphPanel.java
+++ src/CSGraphPanel.java
@@ -4,22 +4,33 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
+import java.util.LinkedList;
 
 
 public class CSGraphPanel extends JPanel {
     // Data vars 
-    
+    int numGraphs = 4;
+    LinkedList<CSGraph> graphList = new LinkedList<CSGraph>();
+    String tempName;
+    CSGraph graph;
+
     // Constructor 
     public CSGraphPanel() {
         // Initialize
-        super();
-        CSGraph myGraph = new CSGraph();
-        
-        
-        
-        // Create subpanels
-        
+    	super();
+    	for(int i = 0; i < numGraphs; i++) {
+    		tempName = ("Graph " + Integer.toString((i+1)));
+    		graph = new CSGraph(tempName);
+    		graph.setSize(400, (600/numGraphs));
+    		graph.setVisible(true);
+    		graphList.add(graph);
+    	}
+
         // Add subpanels with layout
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        while(!graphList.isEmpty()) {
+        	this.add(graphList.poll());
+        }
         
         // Assign colour
         this.setBackground(new Color(48, 48, 54));
