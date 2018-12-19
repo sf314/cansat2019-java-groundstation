900e622df4de784a37ae1be88c8e945393b78f1b
sf314 <sf314@icloud.com>
2018-12-19 14:22:27 -0700

    Add spaces to clean up the code

--- src/CSGraph.java
+++ src/CSGraph.java
@@ -6,19 +6,22 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 public class CSGraph extends JPanel {
+	
 	private int lineWidth;
 	private LinkedList<Double> pointList = new LinkedList<Double>();
 	private String name;
 	private String units;
+	
 	public CSGraph(String name) {
-		lineWidth = 3;
-		units = "";
+		this.lineWidth = 3;
+		this.units = "";
 		this.name = name;
 		JLabel lab = new JLabel(name); this.add(lab);
 		System.out.println("new CSGraph");
 		GenerateTestPoints();
 		System.out.println(pointList.size());
 	}
+	
 	public void paintComponent(Graphics g)
 	{
 		System.out.println("Repainting graph " + name + " with " + pointList.size() + " points");
@@ -28,7 +31,10 @@ public class CSGraph extends JPanel {
 			g.setColor(new Color(50, 200, 50));
 			g.fillOval(i, pointList.get(i).intValue(), lineWidth, lineWidth);
 		}
+		
+		this.setBackground(new Color(42, 42, 50));
 	}
+	
 	public void AddPoints(double a){
 		// System.out.println(a);
 		pointList.add(a);
@@ -36,28 +42,35 @@ public class CSGraph extends JPanel {
 			pointList.remove(); // Remove head of list
 		}
 	}
+	
 	public void GenerateTestPoints(){		
 		for(int i=0;i<200;i++){
 		double temp=(Math.random()*100)+1;	
 		AddPoints(temp);
 		}
 	}
+	
 	public String getName(){
 		return name;
 	}
+	
 	public void setName(String name){
 		this.name = name;
 		JLabel lab = new JLabel(name); this.add(lab);
 	}
+	
 	public String getUnits(){
 		return units;
 	}
+	
 	public void setUnits(String units){
 		this.units = units;
 	}
+	
 	public int getLineWidth() {
 		return lineWidth;
 	}
+	
 	public void setLineWidth(int lineWidth) {
 		this.lineWidth = lineWidth;
 	}
